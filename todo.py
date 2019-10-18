20db7ca0ce899d1db7b177859771aca3c48b19de
Kurt Woodham <kwoodham@gmail.com>
2019-10-18 14:16:41 -0400

    restructuring folders

--- todo.py
+++ todo.py
@@ -358,8 +358,8 @@ def mistake403(code):
 def mistake404(code):
     return 'Sorry, this page does not exist!'
 
-
-debug(True)
-run(reloader=True)
+# debug(True)
+# run(reloader=True)
+run()
 # remember to remove reloader=True and debug(True) when you move your
 # application from development to a productive environment
